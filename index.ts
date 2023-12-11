69eb5f3136a7995102bffff0089a1e3387bae628
Sui Gn <natsynapses@gmail.com>
2023-12-10 18:17:10 -0600

    -
    -

--- index.ts
+++
@@ -1,7 +0,0 @@
-// src/index.js
-export { default as CustomButton } from './components/CustomButton';
-export { default as defaultTheme } from './themes/defaultTheme';
-export { default as useUser } from './utils/useUser';
-export { default as Me } from './components/me/user';
-export * from './components/EntityTable';
-// ... other exports
