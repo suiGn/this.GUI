37b43a006ebb7598b6b6859212e2d2a32f7e85dc
Sui Gn <natsynapses@gmail.com>
2024-01-02 12:42:03 -0500

    mui
    mui

--- src/themes/defaultTheme.ts
+++ src/themes/defaultTheme.ts
@@ -1,22 +1,8 @@
-// src/theme/defaultTheme.ts
+// src/themes/defaultTheme.ts
 import { createTheme } from '@mui/material/styles';
 
-/**
- * The default theme for the this.gui library.
- * Defines the basic styling such as colors, typography, etc.
- * 
- * @returns {Theme} The default MUI theme with customizations.
- */
 const defaultTheme = createTheme({
-  // Define your default theme options here
-  // For example:
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    // ...other options
-  },
-  // ...additional theme options
+  // Customize your default theme here
 });
 
 export default defaultTheme;
